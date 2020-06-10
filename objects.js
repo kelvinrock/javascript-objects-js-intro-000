a541d74045074ac9005243af651089f6dc869cf7
Kelvin Jayaram <kelvin.rock.aplus@gmail.com>
2020-06-10 13:44:59 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {"artist": "song"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-updatePlaylist.song = "artist"
-return updatePlaylist
+return updatePlaylist.song = "artist"
+
 }
